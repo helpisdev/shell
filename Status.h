bdc898b7555c6d0d707407d632801da2ac0c47e6
iliaskap12 <iliaskapareliotis@gmail.com>
2021-08-10 23:12:30 +0300

    Add Status.h

---
+++ Status.h
@@ -0,0 +1,15 @@
+//
+// Created by ilias on 9/8/21.
+//
+
+#ifndef MY_SHELL_STATUS_H
+#define MY_SHELL_STATUS_H
+
+enum class Status
+{
+    Terminate,
+    Continue,
+    NotFound
+};
+
+#endif //MY_SHELL_STATUS_H
